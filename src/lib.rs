53a10ae258f34cd5120e1637cf5a5eaa13429c4f
kuroyasu <xjr1300.04@gmail.com>
2025-02-01 20:00:37 +0900

    Implement the FromStr trait

--- src/lib.rs
+++ src/lib.rs
@@ -20,6 +20,12 @@ pub enum YearMonthError {
     InvalidYear(i32),
     #[error("The month is invalid: {0}")]
     InvalidMonth(u8),
+    #[error("The string format is invalid: {0}")]
+    InvalidFormat(String),
+    #[error("The string format of year is invalid: {0}")]
+    InvalidYearFormat(String),
+    #[error("The string format of month is invalid: {0}")]
+    InvalidMonthFormat(String),
 }
 
 /// 年月操作結果
@@ -48,6 +54,32 @@ impl YearMonth {
     }
 }
 
+impl std::str::FromStr for YearMonth {
+    type Err = YearMonthError;
+
+    /// 年と月をハイフンでつなげた文字列から年月を構築する。
+    ///
+    /// # 引数
+    ///
+    /// * `s` - 年月を生成する文字列
+    ///
+    /// # 戻り値
+    ///
+    /// 年月
+    fn from_str(s: &str) -> YearMonthResult<Self> {
+        let (year, month) = s
+            .split_once('-')
+            .ok_or(YearMonthError::InvalidFormat(s.into()))?;
+        let year = year
+            .parse::<i32>()
+            .map_err(|_| YearMonthError::InvalidYearFormat(year.into()))?;
+        let month = month
+            .parse::<u8>()
+            .map_err(|_| YearMonthError::InvalidMonthFormat(month.into()))?;
+        Self::new(year, month)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -73,5 +105,25 @@ mod tests {
         let ym = YearMonth::new(year, month);
         let error = ym.err().unwrap();
         assert_eq!(error, expected);
+    }
+
+    #[rstest::rstest]
+    #[case("1-1", YearMonth::new(1,1).unwrap())]
+    #[case("1-01", YearMonth::new(1,1).unwrap())]
+    #[case("2025-02", YearMonth::new(2025,2).unwrap())]
+    fn year_month_from_str_ok(#[case] s: &str, #[case] expected: YearMonth) {
+        use std::str::FromStr as _;
+        assert_eq!(YearMonth::from_str(s).unwrap(), expected);
+    }
+
+    #[rstest::rstest]
+    #[case("2025", YearMonthError::InvalidFormat("2025".into()))]
+    #[case("-1", YearMonthError::InvalidYearFormat("".into()))]
+    #[case("xxxx-1", YearMonthError::InvalidYearFormat("xxxx".into()))]
+    #[case("2025-", YearMonthError::InvalidMonthFormat("".into()))]
+    #[case("2025-xx", YearMonthError::InvalidMonthFormat("xx".into()))]
+    fn year_month_from_str_err(#[case] s: &str, #[case] expected: YearMonthError) {
+        use std::str::FromStr as _;
+        assert_eq!(YearMonth::from_str(s).err().unwrap(), expected);
     }
 }
